3708081cfe4934c13995fa487e0dfb2682db6670
xzh-dx <101375932+xzh-dx@users.noreply.github.com>
2024-10-23 08:57:29 +0800

    Add files via upload

---
+++ bin/Debug/net8.0-windows/py/海外安装依赖环境.bat
@@ -0,0 +1,12 @@
+@echo off
+REM 获取批处理文件所在目录并切换到该目录
+cd /d %~dp0
+
+REM 恢复pip使用清华源
+set PIP_INDEX_URL=https://pypi.org/simple/
+
+REM 使用python3.8的pip安装requirements.txt中的所有库
+.\python3.8\python.exe -m pip install -r requirements.txt
+
+REM 暂停，以便查看安装输出
+pause
